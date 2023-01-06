0699444b6f6a557da69662246b72fef99071ccb5
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-01-06 18:20:31 +0630

    feat: add delete method

--- main.go
+++ main.go
@@ -25,14 +25,67 @@ var movies = []Movie{
 			Fullname: "Aung Myat Moe",
 		},
 	},
+	{
+		Id:        "2",
+		Title:     "Iron Man",
+		BoxOffice: "300000",
+		Director: &Director{
+			Fullname: "John Doe",
+		},
+	},
 }
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/movies", getAllMovies)
+	router.POST("/movies", createMovie)
+	router.GET("/movies/:id", getMovieById)
+	router.PUT("/movies/:id", updateMovie)
+	router.DELETE("/movies/:id", deleteMovieById)
 
 	router.Run(":3000")
+}
+
+func deleteMovieById(context *gin.Context) {
+	id := context.Param("id")
+	for idx, movie := range movies {
+		if movie.Id == id {
+			movies = append(movies[:idx], movies[idx+1:]...)
+		}
+	}
+}
+
+func updateMovie(context *gin.Context) {
+	var newUpdatedMovie Movie
+
+	if err := context.BindJSON(&newUpdatedMovie); err != nil {
+		context.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+}
+
+func getMovieById(context *gin.Context) {
+	id := context.Param("id")
+
+	for _, movie := range movies {
+		if movie.Id == id {
+			context.IndentedJSON(http.StatusOK, movie)
+			return
+		}
+	}
+}
+
+func createMovie(context *gin.Context) {
+	var newMovie Movie
+
+	if err := context.BindJSON(&newMovie); err != nil {
+		context.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	movies = append(movies, newMovie)
+	context.IndentedJSON(http.StatusCreated, newMovie)
 }
 
 func getAllMovies(context *gin.Context) {
